895c19d22cd85b412a03fe33ab14ce16d4cacda0
joshua <40035381+joshua-isaac@users.noreply.github.com>
2021-10-22 01:47:59 -0400

    fixed marquee

--- components/SpotifyPlayer.js
+++ components/SpotifyPlayer.js
@@ -1,26 +1,48 @@
 import { ImSpotify } from "react-icons/im";
+import Marquee from "react-fast-marquee";
 
 const SpotifyPlayer = ({ data }) => {
   return (
-    <div className="flex items-center">
-      <p>
-        <ImSpotify style={{ color: "#1ED760" }} className="text-lg mr-3" />{" "}
-      </p>
-      <div>
-        {data?.songUrl ? (
-          <marquee style={{ width: "80%" }} direction="left" scrollamount="3">
-            <div className="col-span-1 flex items-center">
-              <p className="ml-2 text-sm">
-                <span>
-                  {data?.title} - {data?.artist}
-                </span>
-              </p>
-            </div>
-          </marquee>
-        ) : (
-          <p className="ml-2 text-sm">Not Playing</p>
-        )}
-      </div>
+    <div>
+      {data?.songUrl ? (
+        <a href={data.songUrl} className="flex items-center">
+          <p>
+            <ImSpotify style={{ color: "#1ED760" }} className="text-lg mr-3" />{" "}
+          </p>
+          <div>
+            {data?.songUrl ? (
+              <Marquee gradient={false}>
+                <p className="ml-2 text-sm">
+                  <span>
+                    {data?.title} - {data?.artist}
+                  </span>
+                </p>
+              </Marquee>
+            ) : (
+              <p className="ml-2 text-sm">Not Playing</p>
+            )}
+          </div>
+        </a>
+      ) : (
+        <div className="flex items-center">
+          <p>
+            <ImSpotify style={{ color: "#1ED760" }} className="text-lg mr-3" />{" "}
+          </p>
+          <div>
+            {data?.songUrl ? (
+              <Marquee gradient={false} delay={2}>
+                <p className="ml-2 text-sm">
+                  <span>
+                    {data?.title} - {data?.artist}
+                  </span>
+                </p>
+              </Marquee>
+            ) : (
+              <p className="ml-2 text-sm">Not Playing</p>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
