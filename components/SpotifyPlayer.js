1c65836a65571f8a9d7760a106098a48e0c90a16
joshua <40035381+joshua-isaac@users.noreply.github.com>
2021-10-22 01:34:05 -0400

    marquee

--- components/SpotifyPlayer.js
+++ components/SpotifyPlayer.js
@@ -8,7 +8,7 @@ const SpotifyPlayer = ({ data }) => {
       </p>
       <div>
         {data?.songUrl ? (
-          <marquee style={{ width: "65%" }} direction="left" scrollamount="3">
+          <marquee style={{ width: "80%" }} direction="left" scrollamount="3">
             <div className="col-span-1 flex items-center">
               <p className="ml-2 text-sm">
                 <span>
